4463e60af39c8f099e9d78cc4bc39bb39884a45c
Kiran Anand <liteninkiran@hotmail.com>
2023-08-29 15:15:07 +0100

    008 - First Component

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# AngularWithTdd
+# Source
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.2.5.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.3.3.
 
 ## Development server
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 @NgModule({
     declarations: [
         AppComponent,
+        SignUpComponent,
     ],
     imports: [
         BrowserModule,
