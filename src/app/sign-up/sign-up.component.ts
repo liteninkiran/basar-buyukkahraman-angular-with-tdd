e24c0e5cf3b894fb7fac08d26d60d8c488036f70
Kiran Anand <liteninkiran@hotmail.com>
2023-08-30 21:21:22 +0100

    013 - Submitting Request

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -10,6 +10,8 @@ export class SignUpComponent implements OnInit {
     public disabled = true;
     public password = '';
     public confirmPassword = '';
+    public username = '';
+    public email = '';
 
     constructor() { }
 
@@ -23,9 +25,36 @@ export class SignUpComponent implements OnInit {
     }
 
     public onChangeConfirmPassword(event: InputEvent): void {
-        const target: HTMLInputElement = event.target as HTMLInputElement;
-        this.confirmPassword = target.value;
+        this.confirmPassword = (event.target as HTMLInputElement).value;
         this.setDisabled();
+    }
+
+    public onChangeUsername(event: InputEvent) {
+        this.username = (event.target as HTMLInputElement).value;
+    }
+
+    public onChangeEmail(event: InputEvent) {
+        this.email = (event.target as HTMLInputElement).value;
+    }
+
+    public onClickSignUp() {
+        const method = 'POST';
+        const url = '/api/1.0/users';
+        const body = {
+            username: this.username,
+            password: this.password,
+            email: this.email,
+        };
+        const bodyJson = JSON.stringify(body);
+        const headers = {
+            'Content-Type': 'application/json',
+        };
+        const fetchOptions = {
+            method: method,
+            body: bodyJson,
+            headers: headers,
+        };
+        fetch(url, fetchOptions);
     }
 
     private setDisabled(): void {
