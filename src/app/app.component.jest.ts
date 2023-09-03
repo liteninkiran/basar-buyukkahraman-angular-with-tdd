df4354b3fd732277c5fce452833e0f1096638558
Kiran Anand <liteninkiran@hotmail.com>
2023-09-03 21:29:20 +0100

    049 - Client-side Routing - Jest Tests

---
+++ src/app/app.component.jest.ts
@@ -0,0 +1,31 @@
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { render, screen } from '@testing-library/angular';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { routes } from './router/app-router.module';
+import { SharedModule } from './shared/shared.module';
+import { SignUpComponent } from './sign-up/sign-up.component';
+
+const setup = async (path: string): Promise<void> => {
+    const { navigate } = await render(AppComponent, {
+        declarations: [HomeComponent, SignUpComponent],
+        imports: [HttpClientModule, SharedModule, ReactiveFormsModule],
+        routes: routes,
+    });
+    await navigate(path);
+}
+
+describe('Routing', (): void => {
+    const name = `Displays $pageId when path is $path`;
+    const table = [
+        { path: '/', pageId: 'home-page' },
+        { path: '/signup', pageId: 'sign-up-page' },
+    ];
+
+    it.each(table)(name, async ({ path, pageId }): Promise<void> => {
+        await setup(path);
+        const page = screen.queryByTestId(pageId);
+        expect(page).toBeInTheDocument();
+    });
+});
