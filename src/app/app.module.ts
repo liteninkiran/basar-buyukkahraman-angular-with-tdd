1bf6881b75437080add2f1370bdce84bf9d6d4ea
Kiran Anand <liteninkiran@hotmail.com>
2023-09-03 21:11:11 +0100

    047 - Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,21 @@ import { SignUpComponent } from './sign-up/sign-up.component';
 import { HttpClientModule } from '@angular/common/http';
 import { SharedModule } from './shared/shared.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+import { AppRouterModule } from './router/app-router.module';
 
 @NgModule({
     declarations: [
         AppComponent,
         SignUpComponent,
+        HomeComponent,
     ],
     imports: [
         BrowserModule,
         HttpClientModule,
         SharedModule,
         ReactiveFormsModule,
+        AppRouterModule,
     ],
     providers: [],
     bootstrap: [AppComponent],
