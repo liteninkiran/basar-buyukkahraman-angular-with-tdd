0085463bfc78f5e61fe80db8bdfac2bf802f499b
Kiran Anand <liteninkiran@hotmail.com>
2023-09-04 22:15:33 +0100

    056 - Activated Route

--- src/app/activate/activate.component.ts
+++ src/app/activate/activate.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
     selector: 'app-activate',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActivateComponent implements OnInit {
 
-    constructor() { }
+    constructor(private route: ActivatedRoute) { }
 
     public ngOnInit(): void {
+        this.route.params.subscribe((params) => {
+            console.log(params);
+        });
     }
-
 }
