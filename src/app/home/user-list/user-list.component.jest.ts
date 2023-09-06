d3d12e2b741a4dd15d92e3989219e5598b1333fa
Kiran Anand <liteninkiran@hotmail.com>
2023-09-06 16:07:49 +0100

    067 - Next & Previous Page - Jest Tests

--- src/app/home/user-list/user-list.component.jest.ts
+++ src/app/home/user-list/user-list.component.jest.ts
@@ -3,7 +3,8 @@ import { render, screen, waitFor } from '@testing-library/angular';
 import { UserListComponent } from './user-list.component';
 import { setupServer } from 'msw/node';
 import { rest } from 'msw';
-import { getPage } from './user-list.component.spec';
+import { getPage } from './test-helper';
+import userEvent from '@testing-library/user-event';
 
 const url = '/api/1.0/users';
 const resolver = (req: any, res: any, ctx: any) => {
@@ -20,13 +21,96 @@ beforeAll(() => server.listen());
 beforeEach(() => server.resetHandlers());
 afterAll(() => server.close());
 
+const setup = async (): Promise<void> => {
+    await render(UserListComponent, {
+        imports: [HttpClientModule],
+    });
+}
+
 describe('User List', (): void => {
 
     it('Displays three users in a list', async (): Promise<void> => {
-        await render(UserListComponent, {
-            imports: [HttpClientModule],
-        });
+        await setup();
         await waitFor((): void => expect(screen.queryAllByText(/user/).length).toBe(3));
     });
 
+    it('Displays next page button', async (): Promise<void> => {
+        await setup();
+        await screen.findByText('user1');
+        expect(screen.queryByText('next >')).toBeInTheDocument();
+    });
+
+    it('Displays next page after clicking next page button', async (): Promise<void> => {
+        await setup();
+        await screen.findByText('user1');
+        await userEvent.click(screen.getByText('next >'));
+        const firstUserInPage2 = await screen.findByText('user4');
+        expect(firstUserInPage2).toBeInTheDocument();
+    });
+
+    it('Does not display next page at last page', async (): Promise<void> => {
+        await setup();
+        await screen.findByText('user1'); await userEvent.click(screen.getByText('next >'));
+        await screen.findByText('user4'); await userEvent.click(screen.getByText('next >'));
+        await screen.findByText('user7');
+        expect(screen.queryByText('next >')).not.toBeInTheDocument();
+    });
+
+    it('Does not display previous page button at first page', async (): Promise<void> => {
+        await setup();
+        await screen.findByText('user1');
+        expect(screen.queryByText('< previous')).not.toBeInTheDocument();
+    });
+
+    it('Displays previous page button in page 2', async (): Promise<void> => {
+        await setup();
+        await screen.findByText('user1'); await userEvent.click(screen.getByText('next >'));
+        await screen.findByText('user4');
+        expect(screen.queryByText('< previous')).toBeInTheDocument();
+    });
+
+    it('Displays previous page after clicking previous page button', async (): Promise<void> => {
+        await setup();
+        await screen.findByText('user1'); await userEvent.click(screen.getByText('next >'));
+        await screen.findByText('user4'); await userEvent.click(screen.getByText('< previous'));
+        const firstUserInPage1 = await screen.findByText('user1');
+        expect(firstUserInPage1).toBeInTheDocument();
+    });
+
 });
+
+
+/*
+
+
+
+findBy
+------
+Returns a promise which resolves when a matching element is found. The promise 
+is rejected if no elements match or if more than one match is found after a 
+default timeout of 1000 ms. If you need to find more than one element, then use 
+findAllBy.
+
+
+
+getBy
+-----
+Returns the first matching node for a query, and throw an error if no elements 
+match or if more than one match is found. If you need to find more than one 
+element, then use getAllBy.
+
+
+
+queryBy
+-------
+Returns the first matching node for a query, and return null if no elements match. 
+This is useful for asserting an element that is not present. This throws if more 
+than one match is found (use queryAllBy instead).
+
+
+Docs:
+-----
+https://callstack.github.io/react-native-testing-library/docs/api-queries
+https://testing-library.com/docs/queries/about
+
+*/
